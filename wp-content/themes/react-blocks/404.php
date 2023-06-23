692218378b4309f8ef110f98ba00b2cd9e9fbbe8
oleh_kayda <kayda.oleh.95@gmail.com>
2023-06-23 12:56:01 +0300

    fix 404 footer swash padings

--- wp-content/themes/react-blocks/404.php
+++ wp-content/themes/react-blocks/404.php
@@ -13,14 +13,20 @@ get_header();
 	<main id="primary" class="site-main">
         <div class="title-only-hero long-left">
             <div class="container">
-                <div class="page-title" ><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'react-blocks' );?></div>
+                <div class="page-title not-found" >
+                    404. Page Not Found.
+                </div>
+                <div class="page-description not-found">
+                    We can't seem to find the page you're looking for.
+                </div>
+                <div class="not-found-button-wrap not-found">
+                    <a href="<?php echo get_site_url();?>" class="btn btn__md secondary not-found-button">Return Home</a>
+                </div>
             </div>
         </div>
+        <div class="not-found-empty"></div>
 
-		<div>
 
-          <p>404</p>
-        </div>
 
 	</main><!-- #main -->
 
